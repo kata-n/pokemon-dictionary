da98d022bf4386842a01d73e7b4ead89704a972f
kata <kawa.knit813@gmail.com>
2024-01-08 02:52:55 +0900

    add:app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,80 @@
-import './App.css';
-import PokemonThumbnails from './PokemonThumbnails'
+import { useEffect, useState } from "react";
+import { PokemonThumbnails } from "./PokemonThumbnails";
+import { translateToJapanese } from "./util/translate";
 
 function App() {
-  const pokemonData = {
-    id:1,
-    name:'フシギダネ',
-    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    pokeType: '草'
-  }
+  const [allPokemonList, setAllPokemonList] = useState([]);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const createPokemonObject = (results) => {
+    results.forEach((pokemon) => {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+      fetch(pokemonUrl)
+        .then((res) => res.json())
+        .then(async (data) => {
+          const _image = data.sprites.other["official-artwork"].front_default;
+          const _iconImage = data.sprites.other.dream_world.front_default;
+          const _pokeType = data.types[0].type.name;
+          const japanese = await translateToJapanese(data.name, _pokeType);
+          const newPokemonList = {
+            id: data.id,
+            name: data.name,
+            iconImage: _iconImage,
+            image: _image,
+            pokeTsype: _pokeType,
+            jpName: japanese.pokeName,
+            jpType: japanese.pokeType,
+          };
+          setAllPokemonList((currentList) =>
+            [...currentList, newPokemonList].sort((a, b) => a.id - b.id)
+          );
+        });
+    });
+  };
+
+  const getAllPokemonList = () => {
+    setIsLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        // setAllPokemonList(data.results);
+        setUrl(data.next);
+        createPokemonObject(data.results);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getAllPokemonList();
+  }, []);
+
   return (
-    <div className="App">
-      <PokemonThumbnails id={pokemonData.id} name={pokemonData.name} image={pokemonData.image} pokeType={pokemonData.pokeType} />
+    <div className="app-container">
+      <h1>ポケモン図鑑</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemonList.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              name={pokemon.jpName}
+              image={pokemon.image}
+              iconImage={pokemon.iconImage}
+              pokeType={pokemon.jpType}
+              key={index}
+            />
+          ))}
+        </div>
+        {isLoading ? (
+          <div className="load-more">now loading...</div>
+        ) : (
+          <button className="load-more" onClick={getAllPokemonList}>
+            もっとみる！
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/PokemonThumbnails.jsx
+++ src/PokemonThumbnails.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const PokemonThumbnails = () => {
-    return <div>memo:PokemonThumbnails</div>
-}
+export const PokemonThumbnails = ({ id, name, image, iconImage, pokeType }) => {
+  return (
+    <div>
+      <div className="number">
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <img src={iconImage} alt={name} className="icon-image" />
+      <div className="detail-wrapper">
+        <h4>{name}</h4>
+        <h3>{pokeType}</h3>
+      </div>
+    </div>
+  );
+};
